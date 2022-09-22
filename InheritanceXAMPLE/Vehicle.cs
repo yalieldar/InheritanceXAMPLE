a733f82563425726d165e1e1e532c67081dc23db
yalieldar <yali.eldar1@gmail.com>
2022-09-22 10:39:34 +0300

    classes

---
+++ InheritanceXAMPLE/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceXAMPLE
+{
+    internal abstract class Vehicle
+    {
+        protected int wheels;
+
+        public Vehicle (int wheels)
+        {
+            this.wheels = wheels;
+        }
+
+        public int GetWheels()
+        {
+            return this.wheels;
+        }
+        
+        public void SetWheels(int wheels)
+        {
+            this.wheels=wheels;
+        }
+    }
+}
